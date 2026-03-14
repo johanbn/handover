dc44f1da6a7479071c7794f9e44d309881f78404
Johan <johan.norlinder@auticon.com>
2026-03-14 17:38:03 +0100

    clearer example

--- examples/demo.py
+++ examples/demo.py
@@ -48,7 +48,7 @@ async def main() -> None:
     #APP.vector_stores["ruter_store"].chunker.max_chunk_size = 800
     print(APP.model_dump_json(indent=4))
     
-    orch = Orchestrator(APP)
+    orch = Orchestrator(APP, enable_memory=True)
     draw_graph_png(orch.graph, "graph.png")
 
 
