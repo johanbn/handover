f62c55aee5ae70374dc67e7dce6bdd71b5ab4a01
Johan <johan.norlinder@auticon.com>
2026-03-17 12:41:02 +0100

    changed to default chunker

--- src/ruter_chatbot/specs/vector_stores.py
+++ src/ruter_chatbot/specs/vector_stores.py
@@ -1,19 +1,19 @@
 from ruter_chatbot.types.iac.vector_store_spec import VectorStoreSpec
 from ruter_chatbot.specs.providers import ruterwiki_ks_extern
 from ruter_chatbot.specs.embedders import EMBEDDERS
-from ruter_chatbot.specs.chunkers import CHUNKERS
+from ruter_chatbot.specs.chunkers import default_chunker
 
 VECTOR_STORES = {
     "ruter_store": VectorStoreSpec(
         name="ruter_store",
         provider=ruterwiki_ks_extern,
         embedder=EMBEDDERS["nomic-embed-text"],
-        chunker=CHUNKERS["smart_chunker"],
+        chunker=default_chunker,
     ),
     "ruter_store_aws": VectorStoreSpec(
         name="ruter_store_aws",
         provider=ruterwiki_ks_extern,
         embedder=EMBEDDERS["cohere-bedrock-multilingual"],
-        chunker=CHUNKERS["smart_chunker"],
+        chunker=default_chunker,
     ),
 }
